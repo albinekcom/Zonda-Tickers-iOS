972d82cc7607e64e79f931fc3ff15c473321426f
Albin "albinek" Sadowski <albinek@albinek.com>
2017-06-24 09:34:13 +0200

    Add project files

---
+++ BitBay-TickerTests/BitBay_TickerTests.swift
@@ -0,0 +1,36 @@
+//
+//  BitBay_TickerTests.swift
+//  BitBay-TickerTests
+//
+//  Created by Albin Sadowski on 24/06/2017.
+//  Copyright © 2017 Albin Sadowski. All rights reserved.
+//
+
+import XCTest
+@testable import BitBay_Ticker
+
+class BitBay_TickerTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
