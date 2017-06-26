9e6650b7cab045176f5c8e31e670bc1201c0136b
Albin "albinek" Sadowski <albinek@albinek.com>
2017-06-26 21:18:20 +0200

    Add Tickers View Controller

---
+++ BitBay-Ticker/Main/TickersViewController.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+final class TickersViewController: UIViewController {
+    
+    @IBOutlet private weak var tickersTableView: UITableView!
+    
+    // MARK: - Managing View
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
