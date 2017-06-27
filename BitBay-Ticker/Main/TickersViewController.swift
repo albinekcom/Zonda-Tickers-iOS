defc8951b6d5177782e7b0f317ff06809ebb970d
Albin "albinek" Sadowski <albinek@albinek.com>
2017-06-27 16:41:37 +0200

    Add RxSwift

--- BitBay-Ticker/Main/TickersViewController.swift
+++ BitBay-Ticker/Main/TickersViewController.swift
@@ -1,13 +1,29 @@
+import RxSwift
 import UIKit
 
 final class TickersViewController: UIViewController {
     
     @IBOutlet private weak var tickersTableView: UITableView!
     
+    private let disposeBag = DisposeBag()
+    
     // MARK: - Managing View
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        helloRx()
+    }
+    
+    private func helloRx() {
+        Observable
+            .just("It should work!")
+            .subscribe(
+                onNext: { (element) in
+                    print(element)
+                }
+            )
+            .addDisposableTo(disposeBag)
     }
     
 }
