4cb847b718fa3b749e2befe364876c2b840bf185
Albin "albinek" Sadowski <albinek@albinek.com>
2017-06-30 07:47:06 +0200

    Add empty entitlements

---
+++ BitBay-TickerTests/Model/TickerFactoryTests.swift
@@ -0,0 +1,10 @@
+import XCTest
+@testable import BitBay_Ticker
+
+final class TickerFactoryTests: XCTestCase {
+    
+    func testMakeObservableTicker() {
+        // TODO: Fill this method
+    }
+    
+}
