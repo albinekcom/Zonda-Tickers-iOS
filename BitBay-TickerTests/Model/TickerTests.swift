ff74be4555e97704149fb4b7c42df2bd997ec7de
Albin "albinek" Sadowski <albinek@albinek.com>
2017-07-08 22:19:58 +0200

    Add more unit tests for Ticker struct

--- BitBay-TickerTests/Model/TickerTests.swift
+++ BitBay-TickerTests/Model/TickerTests.swift
@@ -46,4 +46,61 @@ final class TickerTests: XCTestCase {
         XCTAssertEqual(8.8, ticker.volume)
     }
     
+    func testInitFromDictionary() {
+        let inputDictionary: [String: Any] = [
+            "max": 1.1,
+            "min": 2.2,
+            "last": 3.3,
+            "bid": 4.4,
+            "ask": 5.5,
+            "vwap": 6.6,
+            "average": 7.7,
+            "volume": 8.8,
+            "name": "btcpln"
+        ]
+        
+        let ticker = Ticker(fromDictionary: inputDictionary)
+        
+        XCTAssertEqual(Ticker.Name.btcpln, ticker.name)
+        XCTAssertEqual("BTC", ticker.baseCurrency)
+        XCTAssertEqual("PLN", ticker.counterCurrency)
+        XCTAssertEqual(1.1, ticker.max)
+        XCTAssertEqual(2.2, ticker.min)
+        XCTAssertEqual(3.3, ticker.last)
+        XCTAssertEqual(4.4, ticker.bid)
+        XCTAssertEqual(5.5, ticker.ask)
+        XCTAssertEqual(6.6, ticker.vwap)
+        XCTAssertEqual(7.7, ticker.average)
+        XCTAssertEqual(8.8, ticker.volume)
+    }
+    
+    func testDictionary() {
+        let jsonDictionary = [
+            "max": 1.1,
+            "min": 2.2,
+            "last": 3.3,
+            "bid": 4.4,
+            "ask": 5.5,
+            "vwap": 6.6,
+            "average": 7.7,
+            "volume": 8.8
+        ]
+        
+        let ticker = Ticker(name: .btcpln, jsonDictionary: jsonDictionary)
+        
+        let expectedDictionary: [String: Any] = [
+            "max": 1.1,
+            "min": 2.2,
+            "last": 3.3,
+            "bid": 4.4,
+            "ask": 5.5,
+            "vwap": 6.6,
+            "average": 7.7,
+            "volume": 8.8,
+            "name": "btcpln"
+        ]
+        
+        XCTAssertEqual(expectedDictionary as NSDictionary, ticker.dictionary as NSDictionary)
+    }
+    
 }
