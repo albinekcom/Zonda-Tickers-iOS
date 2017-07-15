e58e4f89ed9581059201b88542ca3fc0d7c983c9
Albin "albinek" Sadowski <albinek@albinek.com>
2017-07-15 21:53:08 +0200

    Add Rx libraries to Widget target

--- BitBay Ticker Widget/TodayViewController.swift
+++ BitBay Ticker Widget/TodayViewController.swift
@@ -1,15 +1,32 @@
-import UIKit
 import NotificationCenter
+import RxCocoa
+import RxSwift
+import UIKit
 
 final class TodayViewController: UIViewController {
     
-    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet private weak var tableView: UITableView!
+    
+    private let disposeBag = DisposeBag()
     
     // MARK: - Managing View
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view from its nib.
+        
+        setupTableView()
+    }
+    
+    // MARK: - Setting
+    
+    private func setupTableView() {
+        let data = Observable<[(String, String)]>.just([("first element", "Detail 1"), ("second element", "Detail 2"), ("third element", "Detail 3")])
+        
+        data.bind(to: tableView.rx.items(cellIdentifier: "Widget Cell")) { (index, model, cell) in
+            cell.textLabel?.text = model.0
+            cell.detailTextLabel?.text = model.1
+        }
+        .disposed(by: disposeBag)
     }
     
 }
