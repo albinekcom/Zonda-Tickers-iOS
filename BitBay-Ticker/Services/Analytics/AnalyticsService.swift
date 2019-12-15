cd59e76769151839f19e3837207d6c26ecad6841
Albin "albinek" Sadowski <albinek@albinek.com>
2019-12-15 13:14:45 +0100

    Add basic analytics service class

---
+++ BitBay-Ticker/Services/Analytics/AnalyticsService.swift
@@ -0,0 +1,56 @@
+import Foundation
+
+struct AnalyticsService {
+    
+    static var isEnabled: Bool = true
+    static var showConsoleLogs: Bool = false
+    
+    // MARK: - States
+    
+    static func trackAddTickerView() {
+        track(name: "Add_Ticker_View")
+    }
+    
+    static func trackTickersView() {
+        track(name: "Tickers_View")
+    }
+    
+    static func trackTickerDetailsView(parameters: [String: String]) {
+        track(name: "Ticker_Details_View", parameters: parameters)
+    }
+    
+    static func trackEditTickersView() {
+        track(name: "Edit_Tickers_View")
+    }
+    
+    static func trackRequestedRatingView() {
+        track(name: "Requested_Rating_View")
+    }
+    
+    // MARK: - Actions
+    
+    static func trackAddedTicker(parameters: [String: String]) {
+        track(name: "Added_Ticker", parameters: parameters)
+    }
+    
+    static func trackRemovedTicker(parameters: [String: String]) {
+        track(name: "Removed_Ticker", parameters: parameters)
+    }
+    
+    static func trackRefreshedTickers(parameters: [String: String]) {
+        track(name: "Refreshed_Tickers", parameters: parameters)
+    }
+    
+    // MARK: - Tracking
+    
+    private static func track(name: String, parameters: [String: String]? = nil) {
+        if isEnabled {
+            // TODO: Invoke analytics service method here
+        }
+        
+        if showConsoleLogs {
+            print("👣 [TRACKED] \"\(name)\" | Parameters: \(String(describing: parameters))")
+        }
+    }
+    
+}
