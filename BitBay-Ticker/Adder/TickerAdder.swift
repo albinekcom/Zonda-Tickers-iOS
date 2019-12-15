87ab92b6b9567e7f864204c4391c3df64705a9ad
Albin "albinek" Sadowski <albinek@albinek.com>
2019-12-15 13:07:04 +0100

    Improve displaying rows when keyboard is displayed

--- BitBay-Ticker/Adder/TickerAdder.swift
+++ BitBay-Ticker/Adder/TickerAdder.swift
@@ -9,32 +9,31 @@ struct TickerAdder: View {
     
     var body: some View {
         NavigationView {
-            KeyboardHost {
-                List {
-                    TextField(NSLocalizedString("add.ticker.search.placeholder", comment: ""), text: $searchTerm)
-                        .textFieldStyle(RoundedBorderTextFieldStyle())
-                        .disableAutocorrection(true)
-                    ForEach(userData.availableTickersIdentifiersToAdd.filter {
-                        self.searchTerm.isEmpty ? true :
-                            $0.title.localizedCaseInsensitiveContains(self.searchTerm)
-                    }) { tickerIdentifier in
-                        AdderRow(text: Text(tickerIdentifier.title))
-                            .padding()
-                            .onTapGesture {
-                                self.userData.appendAndRefreshTicker(from: tickerIdentifier)
-                                self.userData.removeAvailableToAddTickerIdentifier(tickerIdentifier: tickerIdentifier)
-                            }
+            List {
+                TextField(NSLocalizedString("add.ticker.search.placeholder", comment: ""), text: $searchTerm)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .disableAutocorrection(true)
+                ForEach(userData.availableTickersIdentifiersToAdd.filter {
+                    self.searchTerm.isEmpty ? true :
+                        $0.title.localizedCaseInsensitiveContains(self.searchTerm)
+                }) { tickerIdentifier in
+                    AdderRow(text: Text(tickerIdentifier.title))
+                        .padding()
+                        .onTapGesture {
+                            self.userData.appendAndRefreshTicker(from: tickerIdentifier)
+                            self.userData.removeAvailableToAddTickerIdentifier(tickerIdentifier: tickerIdentifier)
                     }
                 }
-                .navigationBarTitle(Text("add.ticker.title"), displayMode: .inline)
-                .navigationBarItems(
-                    trailing: Button(action: {
-                        self.isPresented.toggle()
-                    }) {
-                        Text("Done") // TODO: Use system button or add Localizable key here
-                    }
-                )
             }
+            .navigationBarTitle(Text("add.ticker.title"), displayMode: .inline)
+            .navigationBarItems(
+                trailing: Button(action: {
+                    self.isPresented.toggle()
+                }) {
+                    Text("Done") // TODO: Use system button or add Localizable key here
+                }
+            )
+            .modifier(AdaptsToSoftwareKeyboard())
         }
         .accentColor(.primary)
     }
