5b1b7344f31d1a0bac2e7487426b06d5e7df6b18
Albin "albinek" Sadowski <albinek@albinek.com>
2020-02-01 13:51:32 +0100

    Clean classes for widget

--- BitBay-Ticker-Widget/TodayViewController.swift
+++ BitBay-Ticker-Widget/TodayViewController.swift
@@ -1,28 +1,19 @@
-//
-//  TodayViewController.swift
-//  BitBay-Ticker-Widget
-//
-//  Created by Albin Sadowski on 01/02/2020.
-//  Copyright © 2020 Albin Sadowski. All rights reserved.
-//
-
-import UIKit
 import NotificationCenter
+import UIKit
 
-class TodayViewController: UIViewController, NCWidgetProviding {
-        
+final class TodayViewController: UIViewController {
+    
+    // MARK: - Managing View
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-        
+    
+}
+
+extension TodayViewController: NCWidgetProviding {
+    
     func widgetPerformUpdate(completionHandler: (@escaping (NCUpdateResult) -> Void)) {
-        // Perform any setup necessary in order to update the view.
-        
-        // If an error is encountered, use NCUpdateResult.Failed
-        // If there's no update required, use NCUpdateResult.NoData
-        // If there's an update, use NCUpdateResult.NewData
-        
         completionHandler(NCUpdateResult.newData)
     }
     
